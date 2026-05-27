92a2fdd840cfd34d1b21a3d6daafda56ce10467f
Yash Kushwaha <37941798+YashKushwaha@users.noreply.github.com>
2026-05-27 17:54:59 +0530

    Added endpoint to login and upload file

--- basic_auth/app.py
+++ basic_auth/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException, status, Response, Request
 import secrets
 from tinydb import TinyDB, Query
 from passlib.context import CryptContext
+from fastapi.responses import JSONResponse
 
 pwd_context = CryptContext(
     schemes=["argon2"],
@@ -18,16 +19,20 @@ app = FastAPI()
 
 database = TinyDB('database.json')
 user_db = database.table('users')
+user_wise_file_db = database.table('files')
+
+sessions = {}
 
 def add_dummy_users_to_db():
     dummy_users = [
-        {'username': 'admin', 'pwd': 'admin123'},
-        {'username': 'user1', 'pwd': 'user123'}
+        {'username': 'admin', 'password': 'admin123'},
+        {'username': 'user1', 'password': 'user123'},
+        {'username': 'string', 'password': 'string'},
     ]
     for user in dummy_users:
         if not user_db.search(Query().username == user['username']):
             username=user['username']
-            password=user['pwd']
+            password=user['password']
             password=pwd_context.hash(password)
             
             user = User(
@@ -53,4 +58,45 @@ async def sign_up(user: User):
     password = pwd_context.hash(user.password)
     user_db.insert(dict(username=user.username, password=password))
     return f"User added: {user.username}"
-     
+
+@app.post('/v1/login') 
+async def login(user: User, response: Response):
+    user_data = user_exists(user.username)
+    print('user_data => ', user_data)
+    # Check if user exists in first place
+    if not user_data:
+        raise HTTPException(401, "User not found")
+    
+    # Check if password matches
+    if not pwd_context.verify(user.password, user_data[0]["password"]):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    session_id = secrets.token_hex(16)
+    print('Session ID generated for user:', session_id)
+    
+    sessions[session_id] = user.username
+    print('Username added to sessions => ', sessions)
+    response = JSONResponse(
+            content="Log In Successful",
+            status_code=200
+        )
+    
+    response.set_cookie(key="session_id", value=session_id, httponly=True)
+    return response
+
+class UploadRequest(BaseModel):
+    filename: str
+
+@app.post('/v1/add_file')
+async def add_file(upload: UploadRequest, request: Request):
+    session_id = request.cookies.get('session_id')
+    print('Session ID from cookie => ', session_id)
+    if not session_id:
+        raise HTTPException(401, "Not logged in")
+    print("Sessions => ", sessions)
+    username = sessions.get(session_id)
+    if not username:
+        raise HTTPException(401, "Invalid session")
+    
+    user_wise_file_db.insert(dict(username=username, filename=upload.filename))
+    return f"File '{upload.filename}' added for user '{username}'"
