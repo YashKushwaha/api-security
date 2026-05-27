a37b5c82f1b671e100dafd2d96af743fdc2ce007
Yash Kushwaha <37941798+YashKushwaha@users.noreply.github.com>
2026-05-27 13:56:21 +0530

    Moved user authentication out of endpoints by creating a separate function to check user login

--- basic_auth/app.py
+++ basic_auth/app.py
@@ -66,3 +66,54 @@ def profile(request: Request):
     return {
         "logged_in_as": username
     }
+
+########################################################
+"""
+In the previous apporaches the endpoint was managing the security
+Now we move to framework injects authenticated identity
+"""
+
+def get_current_user(request:Request):
+    session_id = request.cookies.get('session_id')
+
+    if not session_id:
+        # If there is no session id that means user in not logged in
+        raise HTTPException(401, "Not logged in")
+    
+    username = sessions.get(session_id)
+
+    if not username:
+        """
+        Happens when say user logged out so we deleted the session form our state but browser is still sending the old cookie. e.g employee badge deactivated.
+        Server restart - If session was in memory then after restart / crash all the session will be gone. That's why we use Redis, databases etc
+        Session Expired - Not implemented currently but if implemented expired sessions are removed automatically. e.g. hotel keycard expires after checkout time.
+        Deployment to another server - Load balancer may route request to server B, but the state was created in Server B.
+        Removed during automatic session cleanup
+        Bug in session storage
+        Multiple tabs used -> Logged in from tab A, logged out from Tab B but still trying to user Tab A
+
+        SECURITY RISKS -
+        - Session guessing - someone tries random session IDs that's why secrets.token_hex(16) is important
+        - Stolen old session
+        - Tampered cookie - hacker manually edits the cookie
+        """
+        raise HTTPException(401, "Invalid session")
+    
+    return username
+
+@app.get("/v2/profile")
+def profile(username: str = Depends(get_current_user)):
+
+    return {
+        "logged_in_as": username
+    }
+
+@app.get("/v2/myfiles/{file_name}")
+def get_files(
+    file_name: str,
+    username: str = Depends(get_current_user)
+):
+    return {
+        "user": username,
+        "file": file_name
+    }
